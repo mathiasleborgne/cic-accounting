1cb89c675bd0f4e60b1dc240dca5440ea03c0327
Mathias Le Borgne <mathias.leborgne@gmail.com>
2020-01-12 16:28:46 +0100

    Categories as string + use different CSV to compute final sum

--- cic-accounting/src/main.rs
+++ cic-accounting/src/main.rs
@@ -10,42 +10,19 @@ struct AccountingEntry {
     date_effect: String,
     amount: f32,
     label: String,
-    category: ExpenseCategory,
+    category: String,
 }
 
-#[derive(Debug)]
-#[derive(PartialEq)]
-enum ExpenseCategory {
-    // todo: strings might be better for this purpose
-    Car,
-    Transfer,
-    Unknown,
-}
 
-fn category_to_string(category: &ExpenseCategory) -> String {
-    return match category {
-        Car => "Car".to_string(),
-        Transfer => "Transfer".to_string(),
-        Unknown => "Unknown".to_string(),
-    }
-}
 
-fn string_to_category(string_category: &String) -> ExpenseCategory {
-    return match string_category.as_ref() {
-        "Car" => ExpenseCategory::Car,
-        "Transfer" => ExpenseCategory::Transfer,
-        "Unknown" => ExpenseCategory::Unknown,
-        _ => ExpenseCategory::Unknown,
-    }
-}
 
-fn get_category_from_label(label: &String) -> ExpenseCategory {
+fn get_category_from_label(label: &String) -> String {
     if label.contains("VIR PEL") {
-        return ExpenseCategory::Transfer
+        return "Transfer".to_string()
     } else if label.contains("APRR AUTOROUTE CARTE") {
-        return ExpenseCategory::Car
+        return "Car".to_string()
     }
-    return ExpenseCategory::Unknown
+    return "Unknown".to_string()
 }
 
 fn build_accounting_entry_from_raw_csv_record(record: &HashMap<String, String>) -> AccountingEntry {
@@ -72,7 +49,7 @@ fn build_accounting_entry_from_csv_record_with_categories(record: &HashMap<Strin
             Ok(amount_float) => amount_float,
         }, 
         label: record["Libelle"].clone(),
-        category: string_to_category(&record["Category"]),
+        category: record["Category"].clone(),
     }
 }
 
@@ -84,7 +61,7 @@ fn get_sum_all_amounts(accountings: &Vec<AccountingEntry>) -> f32 {
     return sum_all_amounts
 }
 
-fn get_sum_category(accountings: &Vec<AccountingEntry>, category: ExpenseCategory) -> f32 {
+fn get_sum_category(accountings: &Vec<AccountingEntry>, category: String) -> f32 {
     // todo: not so clean
     let mut sum_category = 0.;
     for accounting_entry in accountings {
@@ -96,7 +73,8 @@ fn get_sum_category(accountings: &Vec<AccountingEntry>, category: ExpenseCategor
 }
 
 fn write_csv_guessed_categories(accountings: &Vec<AccountingEntry>) -> Result<(), Box<dyn Error>> {
-    let mut wtr = Writer::from_path("account_guessed_categories.csv")?;
+    let path = "account_guessed_categories.csv";
+    let mut wtr = Writer::from_path(path)?;
     wtr.write_record(&["Date", "Datedevaleur", "Montant", "Libelle", "Category"]);
     for accounting_entry in accountings {
         wtr.write_record(&[
@@ -104,11 +82,12 @@ fn write_csv_guessed_categories(accountings: &Vec<AccountingEntry>) -> Result<()
             accounting_entry.date_effect.clone(),
             accounting_entry.amount.to_string().clone(),
             accounting_entry.label.clone(),
-            category_to_string(&accounting_entry.category), // todo: this always displays "Car"
-            ])?;
-        } 
-        Ok(())
-    }
+            accounting_entry.category.clone(), // todo: this always displays "Car"
+        ])?;
+    } 
+    println!("Saved file {:?}", path);
+    Ok(())
+}
     
 fn read_csv(csv_path: String, entry_builder_function: &dyn Fn(&HashMap<String, String>)-> AccountingEntry) -> Result<Vec<AccountingEntry>, csv::Error> {
     // pass an entry_builder_function to read csv with or without categories
@@ -125,10 +104,15 @@ fn read_csv(csv_path: String, entry_builder_function: &dyn Fn(&HashMap<String, S
 fn print_accountings(accountings: &Vec<AccountingEntry>) {
     println!("{:#?}", accountings);
     println!("----------------");
-    println!("Car expenses: {:?}", get_sum_category(&accountings, ExpenseCategory::Car));
-    println!("Transfer expenses: {:?}", get_sum_category(&accountings, ExpenseCategory::Transfer));
-    println!("Unknown expenses: {:?}", get_sum_category(&accountings, ExpenseCategory::Unknown));
-    println!("Unknown expenses: {:?}", get_sum_category(&accountings, ExpenseCategory::Unknown));
+    let all_expense_categories = [
+        "Car",
+        "Transfer",
+        "Divers",
+        "Unknown",
+    ];
+    for expense_category in all_expense_categories.iter() {        
+        println!("{:?} expenses: {:?}", expense_category, get_sum_category(&accountings, expense_category.to_string()));
+    }
     println!("----------------");
     println!("Total expenses: {:?}", get_sum_all_amounts(&accountings));
     println!("");
@@ -138,9 +122,11 @@ fn main() -> Result<(), csv::Error> {
     let accountings = read_csv("raw_account.csv".to_string(), &build_accounting_entry_from_raw_csv_record)?;
     print_accountings(&accountings);
     write_csv_guessed_categories(&accountings); 
-        // todo: fix car everywhere
         // todo: guess from older CSVs
-    let accountings_modified = read_csv("account_guessed_categories.csv".to_string(), &build_accounting_entry_from_csv_record_with_categories)?;
+        // todo: check dates
+        // todo: split into 2 files for guessing, then summing
+    println!("Modify {:?} and save it as {:?}", "account_guessed_categories".to_string(), "account_guessed_categories_modified".to_string());
+    let accountings_modified = read_csv("account_guessed_categories_modified.csv".to_string(), &build_accounting_entry_from_csv_record_with_categories)?;
     print_accountings(&accountings_modified);
     Ok(())
 }
