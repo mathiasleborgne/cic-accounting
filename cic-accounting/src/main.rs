73786f2105c0d1ca3bf2143c05465e533ec2fac1
Mathias Le Borgne <mathias.leborgne@gmail.com>
2020-02-17 21:31:51 +0100

    Replace first line (dummy)

--- cic-accounting/src/main.rs
+++ cic-accounting/src/main.rs
@@ -6,7 +6,9 @@ use std::error::Error;
 use std::env;
 use std::fs;
 use std::path::Path;
+use std::fs::File;
 use csv::Writer;
+use std::io::{self, Read, Write};
 use crate::chrono::Datelike; // todo: why?
 use std::num::ParseIntError;
 use regex::Regex;
@@ -179,7 +181,7 @@ fn get_known_labels_categories_map() -> Result<HashMap<String, String>, csv::Err
             },
         }
     }
-    println!("Labels map: {:?}", known_labels_categories_map);
+    // println!("Labels map: {:?}", known_labels_categories_map);
     // example: 
     // known_labels_categories_map.insert(get_label_without_number(&"PAIEMENT CB  CHAVILLE MONOPRIX CARTE ".to_string()), "Courses".to_string());
     Ok(known_labels_categories_map)
@@ -198,6 +200,26 @@ fn collect_args() -> Result<(u32, i32, String, String), ParseIntError> {
     Ok((month, year, action, file_name))
 }
 
+fn replace_first_line(file_name: &String)-> Result<(), io::Error> {
+    // https://stackoverflow.com/questions/27215396/how-to-replace-a-word-in-a-file-in-a-txt
+    // todo: fix data.replace
+    let file_path = Path::new(&file_name);
+
+    // Open and read the file entirely
+    let mut data = String::new();
+    {
+        let mut src = File::open(&file_path)?;
+        src.read_to_string(&mut data)?;
+    }
+    // Run the replace operation in memory
+    let new_data = data.replace("Date,Date de valeur,Montant,Libell�,Solde", "Date,Datedevaleur,Montant,Libelle,Solde");
+
+    // Recreate the file and dump the processed contents to it
+    let mut dst = File::create(&file_path)?;
+    dst.write(new_data.as_bytes())?;
+    Ok(())
+}
+
 fn main() -> Result<(), csv::Error> {
     let (current_month, year, action, file_name) = match collect_args() {
         Err(why) => panic!("Error when collecting arguments, try somethin like \"cargo run 12 2019 guess dummy.csv\": {:?}", why),
@@ -205,6 +227,7 @@ fn main() -> Result<(), csv::Error> {
     };
     match action.as_ref() {
         "guess" => {
+            replace_first_line(&file_name.to_string());
             let known_labels_categories_map = get_known_labels_categories_map()?;
             let build_accounting_entry_from_raw_csv_record_with_cats = 
                 |record: &HashMap<String, String>| 
@@ -224,13 +247,9 @@ fn main() -> Result<(), csv::Error> {
         _ => println!("Action should be guess or sum!"), // todo: better check
     }
         // todo: replace 1st line
-        // todo: guess from older CSVs
-        //   make month/year optional in readcsv
-        //   read all files
         // todo: remove month check from sums funcitons
         // todo: remove unused args for sum action
         // todo: retraitsSO/P should be Retraits
-        // todo: copy modified file into modified_accounts folder after summing
     Ok(())
 }
 
